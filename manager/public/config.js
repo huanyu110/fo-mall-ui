c3dd93a8084fcebb9896a3dae0d405e544e70509
huanyu110 <863423165@qq.com>
2023-05-23 22:01:43 +0800

    ADD: ui建库

---
+++ manager/public/config.js
@@ -0,0 +1,29 @@
+var BASE = {
+    /**
+     * @description api请求基础路径
+     */
+    API_DEV: {
+      common: "http://localhost:8890",
+      buyer: "http://localhost:8888",
+      seller: "http://localhost:8889",
+      manager: "http://localhost:8887"
+    },
+    API_PROD: {
+      common: "http://localhost:8890",
+      buyer: "http://localhost:8888",
+      seller: "http://localhost:8889",
+      manager: "http://localhost:8887"
+    },
+    /**
+     * @description // 跳转买家端地址 pc端
+     */
+    // PC_URL: "https://pc-b2b2c.pickmall.cn",
+    /**
+     * @description  // 跳转买家端地址 wap端
+     */
+    // WAP_URL: "https://m-b2b2c.pickmall.cn",
+    /**
+     *  @description api请求基础路径前缀
+     */
+    PREFIX: "/manager"
+  };
